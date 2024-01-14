2985b282db93c4cc987a4532684ffbcd87ea305e
Praneeth22 <yennampraneeth@gmail.com>
2024-01-14 06:01:40 -0500

    number of steps requried

---
+++ String/1347 minimum no of steps.cpp
@@ -0,0 +1,60 @@
+//1347. Minimum Number of Steps to Make Two Strings Anagram
+//Solved
+//Medium
+//Topics
+//Companies
+//Hint
+//You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
+//
+//Return the minimum number of steps to make t an anagram of s.
+//
+//An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+//
+// 
+//
+//Example 1:
+//
+//Input: s = "bab", t = "aba"
+//Output: 1
+//Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+//Example 2:
+//
+//Input: s = "leetcode", t = "practice"
+//Output: 5
+//Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+//Example 3:
+//
+//Input: s = "anagram", t = "mangaar"
+//Output: 0
+//Explanation: "anagram" and "mangaar" are anagrams. 
+// 
+//
+//Constraints:
+//
+//1 <= s.length <= 5 * 104
+//s.length == t.length
+//s and t consist of lowercase English letters only.
+
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        unordered_map<char, int> ms;
+        unordered_map<char, int> mt;
+        for (auto i : s)
+            ms[i]++;
+        for (auto i : t)
+            mt[i]++;
+         int cnt = 0;
+
+        for (auto el : ms) {
+            char ch = el.first;
+            if (mt.find(ch) != mt.end()) {
+                cnt += max(0, ms[ch] - mt[ch]);
+            } else {
+                cnt += ms[ch];
+            }
+        }
+
+        return cnt;
+    }
+};
