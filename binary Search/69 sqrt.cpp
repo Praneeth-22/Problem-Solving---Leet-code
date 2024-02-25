a34142d9ffdea3141a4015bb73a4b60a4ef2a620
Praneeth22 <yennampraneeth@gmail.com>
2024-02-25 00:21:34 -0500

    sqrt()

---
+++ binary Search/69 sqrt.cpp
@@ -0,0 +1,24 @@
+//69. Sqrt(x)
+//Solved
+//Easy
+//Topics
+//Companies
+//Hint
+//Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+//
+//You must not use any built-in exponent function or operator.
+//
+//For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 1,high = x;
+        while(low <= high){
+            long long mid = low +(-low+high)/2;
+            long long val = (mid*mid);
+            if(val<=x) low =mid+1;
+            else high =mid-1;
+        }
+        return high;
+    }
+};
