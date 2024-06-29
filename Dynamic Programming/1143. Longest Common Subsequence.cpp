5d2320cdeaeefcfa41ed52bf75d01f076e898242
Praneeth22 <yennampraneeth@gmail.com>
2024-06-29 18:23:23 -0400

    long common subseq, 1 sql

--- Dynamic Programming/1143. Longest Common Subsequence.cpp
+++ Dynamic Programming/1143. Longest Common Subsequence.cpp
@@ -38,7 +38,16 @@ public:
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.length(),m = text2.length();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return rec(text1,n-1,text2,m-1,dp);
+        vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
+        // return rec(text1,n-1,text2,m-1,dp);
+        for(int i=0;i<=n;i++) dp[i][0]=0;
+        for(int j=0;j<=m;j++) dp[0][j]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1] == text2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
