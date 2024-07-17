39592509ac7d93be44f156b3737898646c66e0d8
Praneeth22 <yennampraneeth@gmail.com>
2024-07-17 19:37:12 -0400

    1552. Magnetic Force Between Two Balls

---
+++ binary Search/367. Valid Perfect Square.cpp
@@ -0,0 +1,44 @@
+//367. Valid Perfect Square
+//Solved
+//Easy
+//Topics
+//Companies
+//Given a positive integer num, return true if num is a perfect square or false otherwise.
+//
+//A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+//
+//You must not use any built-in library function, such as sqrt.
+//
+// 
+//
+//Example 1:
+//
+//Input: num = 16
+//Output: true
+//Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+//Example 2:
+//
+//Input: num = 14
+//Output: false
+//Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+// 
+//
+//Constraints:
+//
+//1 <= num <= 231 - 1
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        // return sqrt(num)==ceil(sqrt(num));
+
+        int l=0;int r = num;
+        while(l<=r){
+            long mid = l+(r-l)/2;
+            if(mid*mid == num) return true;
+            else if (mid*mid > num) r= mid-1;
+            else l = mid+1;
+        }
+        return false;
+    }
+};
