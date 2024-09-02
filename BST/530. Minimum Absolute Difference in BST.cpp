52cdcc9d41be2011dcdc3e80996f88491d89a0f7
Praneeth22 <yennampraneeth@gmail.com>
2024-09-02 17:14:54 -0400

    530. Minimum Absolute Difference in BST

---
+++ BST/530. Minimum Absolute Difference in BST.cpp
@@ -0,0 +1,56 @@
+//530. Minimum Absolute Difference in BST
+//Solved
+//Easy
+//Topics
+//Companies
+//Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+//
+// 
+//
+//Example 1:
+//
+//
+//Input: root = [4,2,6,1,3]
+//Output: 1
+//Example 2:
+//
+//
+//Input: root = [1,0,48,null,null,12,49]
+//Output: 1
+// 
+//
+//Constraints:
+//
+//The number of nodes in the tree is in the range [2, 104].
+//0 <= Node.val <= 105
+// 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int inOrderTraversal(TreeNode* root,int &diff,int &prev){
+        if(root == NULL) return diff;
+        inOrderTraversal(root->left,diff,prev);
+        if(prev!= -1){
+            diff = min(root->val - prev,diff);
+        }
+        prev = root->val;
+        inOrderTraversal(root->right,diff,prev);
+        return diff;
+
+    }
+    int getMinimumDifference(TreeNode* root) {
+        int diff = INT_MAX,prev = -1;
+        return inOrderTraversal(root,diff,prev);
+    }
+};
