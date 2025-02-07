b29029765355b58d99bd06857aca855563631861
Praneeth22 <yennampraneeth@gmail.com>
2025-02-06 23:39:16 -0500

    optimal solution for 88. Merge Sorted Array

--- Array/88 merge sorted array.cpp
+++ Array/88 merge sorted array.cpp
@@ -36,11 +36,51 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int start = m;
-        int iter=0;
-        for(int i =0;i<n;i++) {
-            nums1[start++] = nums2[i];
+        //brute force approach - 1
+        // int start = m;
+        // int iter=0;
+        // for(int i =0;i<n;i++) {
+        //     nums1[start++] = nums2[i];
+        // }
+        // sort(nums1.begin(),nums1.end());
+         //Brute force approach - 2
+        // vector<int> res;
+        // int i = 0, j = 0;
+        // while (i < m && j < n) {
+        //     if (nums1[i] <= nums2[j]) {
+        //         res.push_back(nums1[i]);
+        //         i++;
+        //     } else {
+        //         res.push_back(nums2[j]); 
+        //         j++;
+        //     }
+        // }
+        // while (i < m) {
+        //     res.push_back(nums1[i]);
+        //     i++;
+        // }
+        // while (j < n) {
+        //     res.push_back(nums2[j]);
+        //     j++;
+        // }
+        // for (int k = 0; k < m + n; k++) {
+        //     nums1[k] = res[k];
+        // }
+       //optimal
+        int i = m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                k--;i--;
+            }else{
+                nums1[k] = nums2[j];
+                j--;k--;
+            }
         }
-        sort(nums1.begin(),nums1.end());
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
     }
 };
